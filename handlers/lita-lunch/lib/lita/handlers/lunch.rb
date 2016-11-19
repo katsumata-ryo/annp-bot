2a3f7189f44c3824641ec4773b5ca6546cc29ca3
Katsumata Ryo <katsumata@bracket.co.jp>
2016-11-19 20:29:02 +0900

    cafe情報を追加

--- handlers/lita-lunch/lib/lita/handlers/lunch.rb
+++ handlers/lita-lunch/lib/lita/handlers/lunch.rb
@@ -3,6 +3,7 @@ module Lita
     class Lunch < Handler
       # insert handler code here
       route /lunch/, :lunch
+      route /cafe/, :cafe
 
       RESTRANTS = [
         { name: "店舗1", tabelog: "https://tabelog.com/kanagawa/A1405/A140507/14000813/", genre: "和食", tags: []},
@@ -12,13 +13,38 @@ module Lita
         { name: "店舗5", tabelog: "https://tabelog.com/kanagawa/A1405/A140507/14000813/", genre: "コンビニ", tags: []},
       ].freeze
 
-      def lunch(response)
+      CAFE = [
+        { name: "Orientar Cafe", tabelog: "https://tabelog.com/kanagawa/A1401/A140101/14003211/", genre: "yokohama", tags: [] },
+        { name: "Gingers Beach", tabelog: "https://tabelog.com/kanagawa/A1401/A140212/14014714/", genre: "yokohama", tags: [] },
+        { name: "Lu's Cafe", tabelog: "https://tabelog.com/kanagawa/A1401/A140101/14003371/", genre: "yokohama", tags: [] },
+        { name: "Roku Cafe", tabelog: "https://tabelog.com/kanagawa/A1401/A140101/14011201/", genre: "yokohama", tags: [] },
+        { name: "24/7 restrant", tabelog: "https://tabelog.com/kanagawa/A1401/A140103/14044853/", genre: "yokohama", tags: [] },
+        { name: "BUKATSUDO", tabelog: "https://tabelog.com/kanagawa/A1401/A140103/14056122/", genre: "yokohama", tags: [] },
+        { name: "Clever Cafe", tabelog: "https://tabelog.com/kanagawa/A1401/A140102/14051539/", genre: "yokohama", tags: [] },
+        { name: "スターバックス 御成町", tabelog: "https://tabelog.com/kanagawa/A1404/A140402/14003447/", genre: "kamakura", tags: [] },
+        { name: "SJO COFFEE", tabelog: "https://tabelog.com/kanagawa/A1404/A140402/14055444/", genre: "kamakura", tags: [] },
+        { name: "Branch Kitchen", tabelog: "https://tabelog.com/kanagawa/A1404/A140402/14050545/", genre: "yokohama", tags: [] },
+      ]
 
+      def lunch(response)
         recommend = RESTRANTS.sample
         response.reply("
-          lita おすすめのランチは#{recommend[:name]}です！
+          #{recommend[:name]} とかどうでしょう？
+          -----
           食べログ: #{recommend[:tabelog]}
           ジャンル: #{recommend[:genre]}
+          -----
+        ")
+      end
+
+      def cafe
+        recommend = CAFE.sample
+        response.reply("
+          #{recommend[:name]} とかどうでしょう？
+          -----
+          食べログ: #{recommend[:tabelog]}
+          ジャンル: #{recommend[:genre]}
+          -----
         ")
       end
 
